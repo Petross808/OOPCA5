8b6ccc78bc95c62c9b56b1f964e1bb9c3ff93948
Petr Šulc <95595719+Petross808@users.noreply.github.com>
2024-03-09 17:32:40 +0000

    Added SQLConnectionDecorator
    - Added SQLConnectionDecorator to reduce the amount of repeated code and make the featues shorter and more readable.

--- src/main/java/org/example/DAO/MySqlDao.java
+++ src/main/java/org/example/DAO/MySqlDao.java
@@ -2,13 +2,11 @@ package org.example.DAO;
 
 import org.example.Exceptions.DaoException;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
-// Taken from oop-data-access-layer-sample-1
 public class MySqlDao
 {
+    // Taken from oop-data-access-layer-sample-1
     public Connection getConnection() throws DaoException
     {
         String driver = "com.mysql.cj.jdbc.Driver";
@@ -35,6 +33,7 @@ public class MySqlDao
         return connection;
     }
 
+    // Taken from oop-data-access-layer-sample-1
     public void freeConnection(Connection connection) throws DaoException
     {
         try
@@ -50,5 +49,73 @@ public class MySqlDao
             System.out.println("Failed to free connection: " + e.getMessage());
             System.exit(1);
         }
+    }
+
+    /*
+        Written by Petr Sulc
+        A decorator method that takes in a lambda function. The lambda function takes in an SQlWrapper
+        which holds the connection, statement and resultSet variables. The method opens a new connection and passes it
+        to the lambda through the SQLWrapper. The lambda is then called and its output saved. The method then closes
+        the connection, statement and resultSet and returns the saved lambda output.
+    */
+    public <T> T SQLConnectionDecorator(CheckedFunction<SQLWrapper,T> statementFunction) throws DaoException
+    {
+
+        SQLWrapper wrapper = new SQLWrapper(null,null,null);
+        T out;
+
+        try {
+            wrapper.connection = this.getConnection();
+            out = statementFunction.apply(wrapper);
+
+        }
+        catch (SQLException e) {
+            throw new DaoException(statementFunction.toString() + "() " + e.getMessage());
+        }
+        finally {
+            try
+            {
+                if (wrapper.result != null)
+                {
+                    wrapper.result.close();
+                }
+                if (wrapper.statement != null)
+                {
+                    wrapper.statement.close();
+                }
+                if (wrapper.connection != null)
+                {
+                    freeConnection(wrapper.connection);
+                }
+            } catch (SQLException e)
+            {
+                throw new DaoException(statementFunction.toString() + "() " + e.getMessage());
+            }
+        }
+        return out;
+    }
+
+    /*
+        Written by Petr Sulc
+        Wrapper object that holds a connection, statement and resultSet.
+        Is used to pass values between the SQLConnectionDecorator and its lambda function.
+    */
+    private class SQLWrapper
+    {
+        Connection connection;
+        PreparedStatement statement;
+        ResultSet result;
+        SQLWrapper(Connection connection, PreparedStatement statement, ResultSet result)
+        {
+            this.connection = connection;
+            this.statement = statement;
+            this.result = result;
+        }
+    }
+
+    // Written by Petr Sulc
+    @FunctionalInterface
+    private interface CheckedFunction<T, R> {
+        R apply(T t) throws DaoException;
     }
 }
